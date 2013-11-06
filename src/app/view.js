e3a07d9f527c62392ce765b2f430be8e7f26610f
Rogério Ferro do Nascimento <rogerioferro@gmail.com>
2013-11-06 15:48:57 -0200

    add pizzas to view

--- src/app/view.js
+++ src/app/view.js
@@ -1,19 +1,22 @@
 define(["app/views/home",
+        "app/views/pizzas",
         "app/views/about",
         "app/views/footer",
         "dojo/_base/window",
         "require",
         "dijit/registry"],
-function(home, about, footer, win, require, registry) {
+function(home, pizza, about, footer, win, require, registry) {
   return {
     //expose screens
     home : home,
+    pizza : pizza,
     about : about,
     footer: footer,
     //createDom
     createDom : function() {
       var body = win.body();
       body.innerHTML = home.getHtml() +
+                       pizza.getHtml() +
                        about.getHtml() +
                        footer.getHtml(); //show in all upper screens
     },
