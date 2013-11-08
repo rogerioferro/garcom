570d86646e57a56bb7d190532124b8a3bf22e354
Mariano Alberto Hexsel <marianohexsel@gmail.com>
2013-11-08 20:18:45 -0200

    Cordova Build files

---
+++ platforms/android/bin/Garom-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/gauderio/projects/garcom/platforms/android/bin/Garom-debug-unaligned.apk : \
+/home/gauderio/projects/garcom/platforms/android/bin/Garom.ap_ \
+/home/gauderio/projects/garcom/platforms/android/bin/classes.dex \
+/home/gauderio/projects/garcom/platforms/android/libs/cordova-3.1.0.jar \
---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,13 @@
+/home/gauderio/projects/garcom/platforms/android/bin/classes.dex : \
+/home/gauderio/projects/garcom/platforms/android/bin/classes/br/com/hcel/garcom/R.class \
+/home/gauderio/projects/garcom/platforms/android/bin/classes/br/com/hcel/garcom/R$attr.class \
+/home/gauderio/projects/garcom/platforms/android/bin/classes/br/com/hcel/garcom/R$drawable.class \
+/home/gauderio/projects/garcom/platforms/android/bin/classes/br/com/hcel/garcom/Garom.class \
+/home/gauderio/projects/garcom/platforms/android/bin/classes/br/com/hcel/garcom/R$string.class \
+/home/gauderio/projects/garcom/platforms/android/bin/classes/br/com/hcel/garcom/R$xml.class \
+/home/gauderio/projects/garcom/platforms/android/bin/classes/br/com/hcel/garcom/BuildConfig.class \
+/home/gauderio/projects/garcom/platforms/android/bin/classes/org/apache/cordova/device/Device.class \
+/home/gauderio/projects/garcom/platforms/android/bin/classes/org/apache/cordova/device/Device$1.class \
+/home/gauderio/projects/garcom/platforms/android/bin/classes/org/apache/cordova/networkinformation/NetworkManager$1.class \
+/home/gauderio/projects/garcom/platforms/android/bin/classes/org/apache/cordova/networkinformation/NetworkManager.class \
+/home/gauderio/projects/garcom/platforms/android/bin/dexedLibs/cordova-3.1.0-e367d7d5bf83cff322f76d58934a9872.jar \
