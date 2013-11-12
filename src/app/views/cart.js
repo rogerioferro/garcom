615e0558ddac599e68dd7c3f766cee4e89e4ed3d
Mariano Alberto Hexsel <marianohexsel@gmail.com>
2013-11-12 15:18:22 -0200

    Cart changes not concluded

--- src/app/views/cart.js
+++ src/app/views/cart.js
@@ -1,5 +1,5 @@
 
-var MAX_VALUE = 99;
+var MAX_VALUE = 99; /*MAX VALUE OF....*/
 
 define(["dojo/text!app/views/cart.html",
         "app/screenClass",
@@ -24,25 +24,28 @@ function(html, screenClass, declare, mobile, domConstruct, registry, query, on, 
         this.DomList = query("ul",this.view.domNode)[0];
         this.DomFooter = query(".cart-footer", this.DomList)[0];
         this.DomTotal = query(".cart-c23", this.DomFooter)[0];
+        /*this.DomPartial = query(".cart-c4", this.DomList)[0];*/
       },
       updateScreen : function(){
         this.clearList();
-        this.addItem(2,'suco de maria mole', 2.00);
-        this.addItem(1,'salada de pirulito', 3.30);
-        this.addItem(1,'sanduiche de macarrão', 10.25);
+        this.addItem(2,'Vinho Sarget de Gruaud Larose 2005', 222.22, 0);
+        this.addItem(1,'Vinho Gruaud Larose 2005', 555.55, 0);
+        this.addItem(1,'Pão com banha', 1.25, 0);
         this.updateTotal();
       },
       clearList: function(){
         query('.cart-list',this.list).forEach(domConstruct.destroy);
         this.listArray.length = 0;
       },
-      addItem : function(quant, desc, price){
-        var obj = {quant:quant, desc:desc, price:price};
+      addItem : function(quant, desc, price, totalprice){
+        var obj = {quant:quant, desc:desc, price:price, totalprice:totalprice};
         this.listArray.push(obj);
         var item = new mobile.ListItem({'class':'cart-list'});
         item.domNode.innerHTML = '<div class="cart-c3"></div>\
-                                   <div class="cart-c2">'+Number(price).toFixed(2)+'</div>\
-                                   <div class="cart-c1"><span>'+desc+'<\span></div>';
+                                  <div class="cart-c1"><span>'+desc+'<\span></div>\
+                                  <div class="cart-c2">'+Number(price).toFixed(2)+'</div>\
+                                  <div class="cart-c4">'+Number(totalprice).toFixed(2)+'</div>'; /**/
+
         var picker = new mobile.ValuePickerSlot({value:quant, labelFrom:1, labelTo:MAX_VALUE, readOnly:true});
         picker.placeAt(query(".cart-c3", item.domNode)[0]);
         on(query('.mblValuePickerSlotPlusButton',picker.domNode)[0],"click",
@@ -67,13 +70,16 @@ function(html, screenClass, declare, mobile, domConstruct, registry, query, on, 
         },picker,obj));
         item.placeAt(this.DomFooter,"before");
       },
+
       updateTotal : function(){
         var total = 0;
         for (var i = 0; i < this.listArray.length; i++){
           var obj = this.listArray[i];
-          total += obj.quant * obj.price;
+          line = obj.quant * obj.price;
+          total += line;
+          this.DomTotal.innerHTML =Number(line).toFixed(2); /*onde insere o valor na lista?*/
         }
-        this.DomTotal.innerHTML = "R$ "+Number(total).toFixed(2);
+        this.DomTotal.innerHTML =Number(total).toFixed(2);
       }
   });
   return new myClass(html);
