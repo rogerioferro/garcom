30e3b4fb262473958c4eff28cb2b660672560584
Mariano Alberto Hexsel <marianohexsel@gmail.com>
2013-11-13 17:12:25 -0200

    Programmatically view creation speed test

--- src/app/views/tests.js
+++ src/app/views/tests.js
@@ -1,10 +1,35 @@
 define(["dojo/text!app/views/tests.html",
         "app/screenClass",
+        "dojox/mobile",
+        "dojo/query",
         "dojo/_base/declare"],
-function(html, screenClass, declare){
+function(html, screenClass, mobile, query, declare){
   var myClass = declare(screenClass,{
       constructor : function(args){
         declare.safeMixin(this, args);
+      },
+
+      start : function(){
+        var roundRectList = new mobile.RoundRectList({
+            label: "RoundRectList",
+            id: "RoundRectList"
+        });
+        this.dom = query("#tests")[0];
+        roundRectList.placeAt(this.dom, "last");
+
+        var listItem = new mobile.ListItem({
+          label:'Pizzas',
+          moveTo:'pizzas'
+        });
+        this.roundRectList = query("#RoundRectList")[0];
+        listItem.placeAt(this.roundRectList, "last");
+
+        var listItem = new mobile.ListItem({
+          label:'Bebidas',
+          moveTo:'drinks'
+        });
+        this.roundRectList = query("#RoundRectList")[0];
+        listItem.placeAt(this.roundRectList, "last");
       }
   });
 
