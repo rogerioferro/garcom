75fd46690e8ae4710e481d6246787a81afbe1df2
Rogério Ferro do Nascimento <rogerioferro@gmail.com>
2013-11-16 22:35:29 -0200

    update login view

--- src/app/views/login.js
+++ src/app/views/login.js
@@ -1,11 +1,12 @@
 define(["app/screenClass",
         "dojo/_base/declare",
+        "dojo/_base/lang",
         "dojo/dom-construct",
         "dojox/mobile/Heading",
         "dojox/mobile/RoundRect",
         "dojox/mobile/TextBox",
         "dojox/mobile/Button"],
-function(screenClass, declare, domConstruct,
+function(screenClass, declare, lang, domConstruct,
          mblHeading, mblRoundRect, mblTextBox, mblButton){
   var view = declare(screenClass,{
     createDom : function(){
@@ -14,33 +15,40 @@ function(screenClass, declare, domConstruct,
       var rect = new mblRoundRect({'class':"center-container"});
 
       var innerBox = "<div class='innerBox'>";
-
+      
+      /*add login input*/
       domConstruct.create(
         'label', {'class':'login-label', 'for':'login-login'}, rect.domNode)
         .innerHTML = "Login (CPF ou e-mail):";
-      new mblTextBox({id:'login-login',placeHolder:"CPF ou e-mail"})
-        .placeAt(domConstruct.place(innerBox, rect.domNode));
+      this.login = new mblTextBox({'id':'login-login',
+                                   'placeHolder':"CPF ou e-mail"});
+      this.login.placeAt(domConstruct.place(innerBox, rect.domNode));
 
+      /*add password input*/
       domConstruct.create(
-        'label', {'class':'login-label', 'for':'login-password'}, rect.domNode)
+        'label', {'class':'login-label','for':'login-password'}, rect.domNode)
         .innerHTML = "Senha:";
-      new mblTextBox({id:'login-password',placeHolder:"senha"})
-        .placeAt(domConstruct.place(innerBox, rect.domNode));
+      this.password = new mblTextBox({'id':'login-password',
+                                      'type': 'password',
+                                      'placeHolder':'senha'});
+      this.password.placeAt(domConstruct.place(innerBox, rect.domNode));
 
       var div = domConstruct.create("div", {'class':'center-content'}, rect.domNode);
       /*add Send Button*/
       new mblButton({label:"ENVIAR", 'class':"mblBlueButton mblBigButton"})
         .placeAt(div)
-        .on("click",function(){
-          console.log("send event");
-        });
+        .on("click", lang.hitch(this, function(){
+          console.log("sending...");
+          console.log('login:'+this.login.value);
+          console.log('password:'+this.password.value);
+        }));
 
       /*add New Register Button*/
       new mblButton({label:"NOVO CADASTRO", 'class':"mblBlueButton mblBigButton"})
         .placeAt(div)
-        .on("click",function(){
+        .on("click", lang.hitch(this, function(){
           console.log("new register event");
-        });
+        }));
       
       this.addChild(rect);
     }
